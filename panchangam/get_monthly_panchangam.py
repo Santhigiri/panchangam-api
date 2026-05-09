e574e4eabb7f92eca2be3529d5c7deeaa234bc08
kamalnathvg <kamalnath7171@gmail.com>
2026-05-09 20:53:08 +0530

    changed monthly panchangam to default to sunrise time

--- panchangam/get_monthly_panchangam.py
+++ panchangam/get_monthly_panchangam.py
@@ -1,11 +1,13 @@
-from datetime import datetime
+from datetime import date, datetime
 from typing import Dict
 import calendar
-from fastapi import Depends
+from fastapi import Query
 import kollavarsham
+from skyfield.api import Topos
 from panchangam.astronomical_calculations import get_panchangam
+from panchangam.get_sunrise_sunset import get_sunrise_sunset
 
-def get_kollavarasham(latitude: float, longitude: float):
+def get_kollavarasham(latitude: float = Query(), longitude: float = Query()):
     return kollavarsham.Kollavarsham(latitude=latitude, longitude=longitude, system="SuryaSiddhanta")
 
 cal = calendar.Calendar(firstweekday=6)
@@ -16,18 +18,23 @@ def get_monthly_panchangam(
         latitude_degrees: float,
         longitude_degrees: float,
         timezone: str,
-        kv: kollavarsham.Kollavarsham = Depends(get_kollavarasham)
+        kv: kollavarsham.Kollavarsham
     )-> Dict:
     data = {}
     for day in cal.itermonthdates(year, month):
         panchangam = {}
+        sunrise_dt, _ = get_sunrise_sunset(
+                date =day,
+                location= Topos(latitude_degrees=latitude_degrees, longitude_degrees = longitude_degrees),
+                timezone=timezone
+        )
         localdt = datetime(
             year=day.year,
             month=day.month,
             day=day.day,
-            hour=0,
-            minute=0,
-            second=0
+            hour=sunrise_dt.hour,
+            minute=sunrise_dt.minute,
+            second=sunrise_dt.second
         )
         panchangam = get_panchangam(
             kv=kv,
