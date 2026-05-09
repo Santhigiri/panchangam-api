5568943a204504bd22aed63945d33a4f54d5ebd1
kamalnathvg <kamalnath7171@gmail.com>
2026-05-09 20:52:38 +0530

    rewrote ayanamsa calculation using swisseph

---
+++ panchangam/get_ayanamsa.py
@@ -0,0 +1,28 @@
+import swisseph as swe
+
+def get_ayanamsa(
+    year: int,
+    month: int = 1,
+    day: int = 1,
+    hour: float = 0.0
+) -> float:
+    """
+    Calculate Lahiri Ayanamsa using Swiss Ephemeris.
+
+    Returns:
+        float: Lahiri ayanamsa in degrees
+    """
+
+    # Set Lahiri ayanamsa mode
+    swe.set_sid_mode(swe.SIDM_LAHIRI)
+
+    # Convert date to Julian Day
+    jd = swe.julday(year, month, day, hour)
+
+    # Get ayanamsa
+    ayanamsa = swe.get_ayanamsa(jd)
+
+    return ayanamsa
+
+
+print(get_ayanamsa(2026,5,2,6))
